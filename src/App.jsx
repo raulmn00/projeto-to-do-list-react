562dda1fdb49e4d07d886b9d60e93ed9ec755dfe
Raul Mariaci Neto <raul.mariaci@gmail.com>
2022-09-06 22:47:20 -0300

    feat: create footer component and styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css';
+import { Footer } from './components/Footer/Footer';
 import { Header } from './components/Header/Header.jsx';
 import { MainContent } from './components/MainContent/MainContent';
 
@@ -8,6 +9,7 @@ function App() {
 			<div className="containerApp">
 				<Header />
 				<MainContent />
+				<Footer />
 			</div>
 		</>
 	);
